609eaf985ca159fbbaa68afee5b30d24227c8097
halfnelson <dpershouse@gmail.com>
2019-03-03 11:09:21 +1000

    xmlns done, working on binding

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import  MagicString  from 'magic-string';
+import MagicString from 'magic-string';
+import parseComponent from 'svelte-component-parser'
+import { Ast, Node } from 'svelte-component-parser/interfaces';
 
 export interface SveltePreprocessorInput {
     content: string;
@@ -12,16 +14,73 @@ export interface SveltePreprocessorDefinition {
     markup: (source: SveltePreprocessorInput) => SveltePreprocessorOutput;
 }
 
+type NodeVisitorAction = (node: Node, depth: number, index: number) => void;
+
+function walkNodes(node: Node, action: NodeVisitorAction, current_depth: number = 0, current_index: number = 0) {
+    try {
+        action(node, current_depth, current_index)
+    } catch (e) {
+        throw new Error(`error walking ${node.type} node at depth ${current_depth} index ${current_index} \n ${e.message}`);
+    }
+
+    if (!node.children)
+        return;
+
+    for (let index of node.children.keys()) {
+        walkNodes(node.children[index], action, current_depth + 1, index)
+    }
+}
+
+function isWhiteSpace(char: string) {
+    return char == ' ' || char == '\n' || char == '\t' || char == '\r';
+}
+
+function insertAttributeToElement(element: Node, attributeString: string, src: string, dest: MagicString) {
+    let insertIdx = src.indexOf(element.name, element.start) + element.name.length;
+    let insertStr = ` ${attributeString}` + (isWhiteSpace(src[insertIdx]) ? '' : ' ');
+    dest.appendRight(insertIdx, insertStr);
+}
+
+function addXmlNamespaceToRootElements(ast: Ast, src: string, dest: MagicString) {
+    if (!ast.html) return;
+    walkNodes(ast.html, (node: Node, depth: number, index: number) => {
+        if (node.type == 'Element' && depth == 1) {
+            let xmlnsAttr = node.attributes.find((attr: any) => attr.name == 'xmlns');
+            if (!xmlnsAttr) {
+                insertAttributeToElement(node, 'xmlns="tns"', src, dest)
+            }
+        }
+    });
+}
+
+function expandBindOnTagElements(ast: Ast, src: string, dest: MagicString) {
+    if (!ast.html) return;
+    walkNodes(ast.html, (node: Node, depth: number, index: number) => {
+        if (node.type == 'Element') {
+            console.log(node);
+        }
+    });
+}
+
 export default function preprocess() {
     return {
-        markup: function(source: SveltePreprocessorInput ): SveltePreprocessorOutput {
-            var src = new MagicString(source.content);
-            var map = src.generateMap({
+        markup: function (source: SveltePreprocessorInput): SveltePreprocessorOutput {
+            //input
+            var out = new MagicString(source.content);
+            var ast = parseComponent(source.content, { filename: source.file })
+            var src = source.content;
+
+            //transforms
+            addXmlNamespaceToRootElements(ast, src, out);
+            expandBindOnTagElements(ast, src, out);
+            
+            //output
+            var map = out.generateMap({
                 source: source.file,
                 file: source.file + ".map",
                 includeContent: true
             });
-            return { code: src.toString(), map: map.toString() };
+            return { code: out.toString(), map: map.toString() };
         }
     }
 }
--- test/preprocessFixture.ts
+++ test/preprocessFixture.ts
@@ -3,13 +3,59 @@ import * as assert from 'assert';
 
 describe("preprocess", function () {
     let p: SveltePreprocessorDefinition;
+   
+    function testMarkup(input: string, expected: string) {
+        let res = p.markup({ content: input, file: 'Index.svelte' });
+        assert.equal(res.code, expected);
+    }
+
+    function testElementMarkup(input: string, expected: string) {
+        let content =  `<page xmlns="tns">\n${input}\n</page>`;
+        let res = p.markup({ content: content, file: 'Index.svelte' });
+
+        let codeLines = res.code.split('\n');
+        codeLines.shift();
+        codeLines.pop()
+
+        assert.equal(codeLines.join('\n'), expected);
+    }
 
     beforeEach(function () {
         p = preprocess();
     });
 
     it('should add process empty file', function () {
-        let res = p.markup({ content: '', file: 'Index.svelte' });
-        assert.equal(res.code, "");
+        testMarkup('', '');
     });
+
+    describe("preprocess adds xmlns attribute", function () {
+        it('should add xmlns attribute to single root element', function () {
+            testMarkup('<page class="root-page"/>', '<page xmlns="tns" class="root-page"/>');
+        });
+
+        it('should add xmlns attribute to single root element with no attributs', function () {
+            testMarkup('<page/>', '<page xmlns="tns" />');
+            testMarkup('<page></page>', '<page xmlns="tns" ></page>');
+        });
+
+        it('should add xmlns attribute and handle whitespace', function() {
+            testMarkup('<page\n></page>', '<page xmlns="tns"\n></page>');
+            testMarkup('<page\n  class="hi"></page>', '<page xmlns="tns"\n  class="hi"></page>');
+        })
+    });
+
+    describe("preprocess adds expands bind: attribute", function () {
+        it('should not expand bind on svelte components', function() {
+            testElementMarkup('<SvelteComponent bind:a={this} />','<SvelteComponent bind:a={this} />')
+        });
+
+        it('should not expand bind on svelte tags', function() {
+            testElementMarkup('<svelte:component this={SvelteComponent} bind:a={this} />','<svelte:component this={SvelteComponent} bind:a={this} />')
+        });
+
+        it('should expand bind on regular tags', function() {
+            testElementMarkup('<textInput bind:text={email} />','<textInput text={email} on:textChange="{(e) => email = e.value} />')
+        });
+    });
+    
 })
