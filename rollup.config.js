af7bd15596e08184c1ff6c60441a272db5e37230
halfnelson <dpershouse@gmail.com>
2020-04-22 20:35:55 +1000

    v0.1.8 output umd for REPL

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default [
 	{
 		input: 'src/index.ts',
 		output: [
-			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.main, format: 'umd', name: "svelteNativePreprocessor" },
 		],
 		plugins: [
 			resolve(), 
